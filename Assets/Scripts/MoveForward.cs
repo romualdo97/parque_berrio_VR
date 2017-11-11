a9352024f4c38e2528b9c3428163b5b80bd4c1ef
Romualdo Villalobos <romualdorojo97@gmail.com>
2017-11-11 17:49:34 -0500

    Decorations and final indications ready

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -15,8 +15,18 @@ public class MoveForward : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        gameObject.transform.position += gameObject.transform.forward * speed;
-        gameObject.transform.Rotate(new Vector3(1f, 0f, 0f), Mathf.Sin(Time.realtimeSinceStartup*5f) * 0.08f);
-        gameObject.transform.Rotate(new Vector3(0f, 0f, 1f), Mathf.Sin(Time.realtimeSinceStartup*5f)*0.06f);
+        if (gameObject.transform.position.x > 508)
+        {
+            gameObject.transform.position += gameObject.transform.forward * speed;
+            gameObject.transform.Rotate(new Vector3(1f, 0f, 0f), Mathf.Sin(Time.realtimeSinceStartup * 5f) * 0.08f);
+            gameObject.transform.Rotate(new Vector3(0f, 0f, 1f), Mathf.Sin(Time.realtimeSinceStartup * 5f) * 0.06f);
+        }
+
+        if (Input.GetKey(KeyCode.R)) Reset();
+    }
+
+    void Reset()
+    {
+        gameObject.transform.position = new Vector3(700f, -8f, 241f);
     }
 }
